0894fa447be78d4b77b6a6975bf1eb0f43ed8db4
olivia <olivia.brantley9@gmail.com>
2023-06-07 10:10:17 -0700

    started on header

---
+++ src/components/header.jsx
@@ -0,0 +1,21 @@
+import '../App.css'
+
+function Header() {
+  
+    return (
+      <>
+      <div className='header'>
+        <div>OlvBrntly</div>
+        <div className="header-right">
+            <div>About</div>
+            <div>Projects</div>
+            <div>Resume</div>
+            <div>Contact</div>
+        </div>
+      </div>
+      </>
+    )
+  }
+  
+  export default Header
+  
